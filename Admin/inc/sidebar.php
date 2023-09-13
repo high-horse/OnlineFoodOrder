f636cff5004dba2f81f941e74c97c6b4a34a9136
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2023-09-13 19:32:56 +0100

    order report

--- Admin/inc/sidebar.php
+++ Admin/inc/sidebar.php
@@ -1,54 +1,54 @@
 <!--sidebar start-->
-  <aside>
-    <div id="sidebar" class="nav-collapse ">
-      <!-- sidebar menu start-->
-      <ul class="sidebar-menu">
-        <li class="active">
-          <a class="" href="Dashboard.php">
-                        <i class="icon_house_alt"></i>
-                        <span>Dashboard</span>
-                    </a>
-        </li>
-        <li class="sub-menu">
-          <a href="javascript:;" class="">
-                        <i class="icon_document_alt"></i>
-                        <span>Category Option</span>
-                        <span class="menu-arrow arrow_carrot-right"></span>
-                    </a>
-          <ul class="sub">
-            <li><a class="" href="add_category.php">Add Category</a></li>
-            <li><a class="" href="Display_category.php">Display Category</a></li>
-          </ul>
-        </li>
-        <li class="sub-menu">
-          <a href="javascript:;" class="">
-                        <i class="icon_document_alt"></i>
-                        <span>Product Option</span>
-                        <span class="menu-arrow arrow_carrot-right"></span>
-                    </a>
-          <ul class="sub">
-            <li><a class="" href="add_product.php">Add Product</a></li>
-            <li><a class="" href="Display_product.php">Display Product</a></li>
-          </ul>
-        </li>
+<aside>
+  <div id="sidebar" class="nav-collapse ">
+    <!-- sidebar menu start-->
+    <ul class="sidebar-menu">
+      <li class="active">
+        <a class="" href="Dashboard.php">
+          <i class="icon_house_alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="sub-menu">
+        <a href="javascript:;" class="">
+          <i class="icon_document_alt"></i>
+          <span>Category Option</span>
+          <span class="menu-arrow arrow_carrot-right"></span>
+        </a>
+        <ul class="sub">
+          <li><a class="" href="add_category.php">Add Category</a></li>
+          <li><a class="" href="Display_category.php">Display Category</a></li>
+        </ul>
+      </li>
+      <li class="sub-menu">
+        <a href="javascript:;" class="">
+          <i class="icon_document_alt"></i>
+          <span>Product Option</span>
+          <span class="menu-arrow arrow_carrot-right"></span>
+        </a>
+        <ul class="sub">
+          <li><a class="" href="add_product.php">Add Product</a></li>
+          <li><a class="" href="Display_product.php">Display Product</a></li>
+        </ul>
+      </li>
 
-        <li class="sub-menu">
-          <a href="javascript:;" class="">
-                        <i class="icon_document_alt"></i>
-                        <span>Reports</span>
-                        <span class="menu-arrow arrow_carrot-right"></span>
-                    </a>
-          <ul class="sub">
-            <li><a class="" href="billwisereport.php">BillWise Report</a></li>
-            <li><a class="" href="customerwisereport.php">Customerwise Report</a></li>
-            <li><a class="" href="revenueeport.php">Revenue Report</a></li>
-            <li><a class="" href="revenueeport.php">Order Report</a></li>
+      <li class="sub-menu">
+        <a href="javascript:;" class="">
+          <i class="icon_document_alt"></i>
+          <span>Reports</span>
+          <span class="menu-arrow arrow_carrot-right"></span>
+        </a>
+        <ul class="sub">
+          <li><a class="" href="billwisereport.php">BillWise Report</a></li>
+          <li><a class="" href="customerwisereport.php">Customerwise Report</a></li>
+          <li><a class="" href="revenuereport.php">Revenue Report</a></li>
+          <li><a class="" href="orderreport.php">Order Report</a></li>
 
-          </ul>
-        </li>
+        </ul>
+      </li>
 
-      </ul>
-      <!-- sidebar menu end-->
-    </div>
-  </aside>
-  <!--sidebar end-->
+    </ul>
+    <!-- sidebar menu end-->
+  </div>
+</aside>
+<!--sidebar end-->
---
+++ Admin/revenuereport.php
@@ -0,0 +1,183 @@
+<?php include 'inc/head.php';
+include 'inc/header.php';
+include 'inc/sidebar.php'; ?>
+<?php include '../classes/Product.php'; ?>
+<?php include_once '../helpers/Format.php'; ?>
+<?php
+$pd = new Product();
+$fm = new Format();
+$db = new Database();
+?>
+
+
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+
+<!--main content start-->
+<section id="main-content">
+    <section class="wrapper">
+        <!--overview start-->
+        <div class="row">
+            <div class="col-lg-12">
+                <ol class="breadcrumb">
+                    <li><i class="fa fa-home"></i><a href="Dashboard.php">Home</a></li>
+                    <li><i class="fa fa-laptop"></i>Orders</li>
+                </ol>
+            </div>
+        </div>
+        <?php
+        if (isset($delpro)) {
+            echo $delpro;
+        }
+        ?>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <section class="panel">
+                    <header class="panel-heading">
+                        Today's Orders
+                    </header>
+                    <div class="table-responsive">
+                        <table class="table table-striped table-advance table-hover">
+                            <tbody>
+                                <tr>
+                                    <th> Item</th>
+                                    <th> Quantity</th>
+                                    <th> Ordered by</th>
+                                    <th> Address</th>
+
+                                    <th> Customer ID</th>
+                                    <th> Time</th>
+                                </tr>
+                                <?php
+                                $query = "SELECT b.billId,c.productName as Item, c.quantity as Quantity,b.fname as Buyer,b.lname,b.fulladdress as Address,b.cid,b.sid,b.cdate FROM tbl_bill_details as b, tbl_cart as c
+                     WHERE b.sid = c.sId AND cdate>=CURDATE()";
+                                $inserted_row = $db->select($query);
+
+                                if ($inserted_row) {
+                                    $i = 0;
+                                    while ($result = $inserted_row->fetch_assoc()) {
+                                        $i++;
+                                ?>
+                                        <tr>
+                                            <td><?php echo $result['Item']; ?></td>
+                                            <td><?php echo $result['Quantity'] ?></td>
+                                            <td><?php echo $result['Buyer'] ?></td>
+                                            <td><?php echo $result['Address'] ?></td>
+                                            <td><?php echo $result['cid'] ?></td>
+
+                                            <div class="btn-group">
+                                                <td> <?php echo $result['cdate'] ?></td>
+                                            </div>
+                                        </tr>
+                                <?php }
+                                } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <br />
+        <hr />
+        <br />
+        <div class="row">
+            <div class="col-lg-12">
+                <section class="panel">
+                    <header class="panel-heading">
+                        <b> Bill Wise Report</b>
+                    </header>
+                    <div class="table-responsive">
+                        <table class="table table-striped table-advance table-hover" id="bill-wise-report">
+                            <thead>
+                                <tr>
+                                    <th>SN</th>
+                                    <th> Ordered by</th>
+                                    <th> Address</th>
+                                    <th> Customer ID</th>
+                                    <th> Time</th>
+                                    <th>Items With Quantity</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <?php
+                                $query = "SELECT b.cid, b.fname AS Buyer, b.lname, b.fulladdress AS Address, b.billId, b.sid, b.cdate, GROUP_CONCAT(CONCAT(c.productName, ' (', c.quantity, ')') SEPARATOR ', ') AS ItemsWithQuantity FROM tbl_bill_details AS b JOIN tbl_cart AS c ON b.sid = c.sId JOIN tbl_customer AS tc ON tc.id = b.cid GROUP BY b.cid, b.fname, b.lname, b.fulladdress, b.billId, b.sid, b.cdate";
+                                //   print_r($query);die;
+                                $inserted_row = $db->select($query);
+
+                                if ($inserted_row) {
+                                    $i = 0;
+                                    while ($result = $inserted_row->fetch_assoc()) {
+                                        $i++;
+                                ?>
+                                        <tr>
+                                            <td><?php echo $i + 1; ?></td>
+                                            <td><?php echo $result['Buyer'] ?></td>
+                                            <td><?php echo $result['Address'] ?></td>
+                                            <td><?php echo $result['cid'] ?></td>
+                                            <td><?php echo $result['cdate'] ?></td>
+                                            <td><?php echo $result['ItemsWithQuantity'] ?></td>
+                                        </tr>
+                                <?php }
+                                } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </section>
+    <!--main content end-->
+</section>
+<!-- container section start -->
+
+<style>
+    .dt-buttons {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 10px;
+        justify-content: flex-end;
+    }
+
+    .buttons-copy,
+    .buttons-excel,
+    .buttons-pdf {
+        background-color: #007bff;
+        color: #fff;
+        border: none;
+        padding: 6px 12px;
+        /* Adjust padding to make buttons smaller */
+        border-radius: 4px;
+        cursor: pointer;
+        font-size: 14px;
+        /* Adjust font size to make buttons smaller */
+    }
+
+    .buttons-copy:hover,
+    .buttons-excel:hover,
+    .buttons-pdf:hover {
+        background-color: #0056b3;
+    }
+</style>
+
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
+<script>
+    $(document).ready(function() {
+        new DataTable('#bill-wise-report', {
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'excel', 'pdf'
+            ]
+        });
+    });
+</script>
+
+<?php include 'inc/footer.php'; ?>
